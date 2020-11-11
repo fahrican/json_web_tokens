e088c8d9f88f3d11436f14a65cfb47c1c27e0eaa
Fahri MacBook <fahri-can@web.de>
2020-11-11 11:41:40 +0100

    Commit rest of project.

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request
+from flask_jwt_extended import (JWTManager,
+                                create_access_token, create_refresh_token,
+                                set_access_cookies, set_refresh_cookies,
+                                jwt_required, jwt_refresh_token_required,
+                                get_jwt_identity, unset_jwt_cookies)
+
+app = Flask(__name__)
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_COOKIE_SECURE'] = False
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
+app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/refresh'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+app.config['JWT_SECRET_KEY'] = 'fahri_can'
+
+jwt = JWTManager(app)
+
+
+@app.route('/token/auth', methods=['GET'])
+def login():
+    username = request.args.get('username', None)
+    password = request.args.get('password', None)
+    if username != 'admin' or password != 'nimda':
+        return jsonify({'login': False}), 401
+    access_cookies = create_access_token(identity=username)
+    refresh_cookies = create_refresh_token(identity=username)
+    resp = jsonify({'login': True})
+    set_access_cookies(resp, access_cookies)
+    set_refresh_cookies(resp, refresh_cookies)
+    return resp, 200
+
+
+@app.route('/token/refresh', methods=['POST'])
+@jwt_refresh_token_required
+def refresh():
+    user = get_jwt_identity()
+    access_token = create_access_token(identity=user)
+    resp = jsonify({'refresh': True})
+    set_access_cookies(resp, access_token)
+    return resp, 200
+
+
+@app.route('/token/remove', methods=['POST'])
+def logout():
+    resp = jsonify({'logout': True})
+    unset_jwt_cookies(resp)
+    return resp
+
+
+@app.route('/api/example', methods=['GET'])
+@jwt_required
+def protected():
+    user = get_jwt_identity()
+    return jsonify({'hello': user}), 200
+
+
+if __name__ == '__main__':
+    app.run()
